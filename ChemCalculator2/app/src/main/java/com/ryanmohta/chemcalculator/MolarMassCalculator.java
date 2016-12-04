0ec2a1e003b655b931abf9d9fc257c6d9f86840c
ryanmohta <ryanmohta@gmail.com>
2016-12-03 18:40:27 -0800

    cool

--- ChemCalculator2/app/src/main/java/com/ryanmohta/chemcalculator/MolarMassCalculator.java
+++ ChemCalculator2/app/src/main/java/com/ryanmohta/chemcalculator/MolarMassCalculator.java
@@ -7,11 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MolarMassCalculator extends Fragment {
-
+    Button button;
+    EditText inputText;
 
     public MolarMassCalculator() {
         // Required empty public constructor
@@ -24,7 +27,16 @@ public class MolarMassCalculator extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_molar_mass_calculator, container, false);
+        View view = inflater.inflate(R.layout.fragment_molar_mass_calculator, container, false);
+        button = (Button)view.findViewById(R.id.button);
+        inputText = (EditText)view.findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Hi", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
 
